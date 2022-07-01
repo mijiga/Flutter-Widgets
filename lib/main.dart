df7e435c0fc8e4a37238dfbeb3c4320b8ebb34e5
Steve Chikwiri <37661480+mijiga@users.noreply.github.com>
2022-07-02 01:58:21 +0300

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'dart:ui';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:modal_bottom_sheet/modal_bottom_sheet.dart';
 import 'package:widgets/util/util.dart';
 
 void main() {
